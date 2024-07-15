a32379a09e5ef3cfc563ef434e67ebd904bc02cc
Geetika37 <geetikadinesh3@gmail.com>
2024-07-15 15:15:23 +0530

    message

--- lib/screens/home/controller/categoryprod_controller.dart
+++ lib/screens/home/controller/categoryprod_controller.dart
@@ -1,3 +1,51 @@
+import 'dart:convert';
+import 'package:get/get.dart';
+import 'package:royal_fuji_star/services/api_baseurl.dart';
+import 'package:royal_fuji_star/services/token.dart';
+import 'package:http/http.dart' as http;
+
+class CategoryProductController extends GetxController {
+  var isLoading = false.obs;
+  var product = <dynamic>[].obs;
+  var errorMessage = ''.obs;
+
+  Future<void> categoryProduct(int id) async {
+    isLoading(true);
+    errorMessage.value = ''; // Clear previous error message
+    product.clear(); // Clear previous data
+
+    try {
+      final token = await TokenKey.getValue('token');
+      final url = Uri.parse(
+          '${APIConstants.baseUrl}/api/products-find-category-products/$id?&title=desc');
+      final response = await http.get(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      final jsonResponse = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        if (jsonResponse['data'] != null) {
+          product.value = jsonResponse['data'];
+        } else {
+          errorMessage.value = 'Error: No data in response';
+        }
+      } else {
+        errorMessage.value = 'Error: ${jsonResponse['error']['message']}';
+      }
+    } catch (e) {
+      errorMessage.value = 'Error: ${e.toString()}';
+    } finally {
+      isLoading(false);
+    }
+  }
+}
+
 // import 'dart:convert';
 
 // import 'package:get/get.dart';
@@ -50,51 +98,4 @@
 // }
 
 
-import 'dart:convert';
-import 'package:get/get.dart';
-import 'package:royal_fuji_star/services/api_baseurl.dart';
-import 'package:royal_fuji_star/services/token.dart';
-import 'package:http/http.dart' as http;
-
-class CategoryProductController extends GetxController {
-  var isLoading = false.obs;
-  var product = <dynamic>[].obs;
-  var errorMessage = ''.obs;
-
-  Future<void> categoryProduct(int id) async {
-    isLoading(true);
-    errorMessage.value = ''; // Clear previous error message
-    product.clear(); // Clear previous data
-
-    try {
-      final token = await TokenKey.getValue('token');
-      final url = Uri.parse(
-          '${APIConstants.baseUrl}/api/products-find-category-products/$id?&title=desc');
-      final response = await http.get(
-        url,
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': 'Bearer $token',
-        },
-      );
-
-      final jsonResponse = jsonDecode(response.body);
-
-      if (response.statusCode == 200) {
-        if (jsonResponse['data'] != null) {
-          product.value = jsonResponse['data'];
-        } else {
-          errorMessage.value = 'Error: No data in response';
-        }
-      } else {
-        errorMessage.value = 'Error: ${jsonResponse['error']['message']}';
-      }
-    } catch (e) {
-      errorMessage.value = 'Error: ${e.toString()}';
-    } finally {
-      isLoading(false);
-    }
-  }
-}
 
