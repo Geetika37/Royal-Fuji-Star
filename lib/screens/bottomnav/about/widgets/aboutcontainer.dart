4ed06eee6f8ad0081009d116f6ca95a93df9d14a
Geetika37 <geetikadinesh3@gmail.com>
2024-07-16 12:50:39 +0530

    message

--- lib/screens/bottomnav/about/widgets/aboutcontainer.dart
+++ lib/screens/bottomnav/about/widgets/aboutcontainer.dart
@@ -1,4 +1,101 @@
 
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:royal_fuji_star/constants/size.dart';
+// import 'package:royal_fuji_star/screens/bottomnav/about/widgets/aboutgridview.dart';
+// import 'package:royal_fuji_star/screens/bottomnav/about/widgets/contactinfo.dart';
+// import 'package:royal_fuji_star/screens/bottomnav/about/widgets/emailinfo.dart';
+// import 'package:royal_fuji_star/screens/bottomnav/about/widgets/videoplayer.dart';
+// import 'package:royal_fuji_star/utils/appcolor.dart';
+// import 'package:royal_fuji_star/utils/textcustom.dart';
+
+// class Aboutcontainer extends StatelessWidget {
+//   const Aboutcontainer({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       // height: screenHeight * 0.58,
+//       height: ScreenSize.getHeight(context) * 0.58,
+//       width: ScreenSize.getWidth(context),
+//       decoration: const BoxDecoration(
+//           color: Appcolor.white,
+//           borderRadius: BorderRadius.only(
+//               topLeft: Radius.circular(45), topRight: Radius.circular(45))),
+//       child: Padding(
+//         padding: const EdgeInsets.all(15.0),
+//         child: SingleChildScrollView(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               SizedBox(height: screenHeight * 0.02),
+//               CustomTitle(
+//                   fontWeight: FontWeight.w500,
+//                   textHeading: 'abouttext1'.tr,
+//                   fontSize: 16,
+//                   color: Appcolor.buttonColor),
+//               SizedBox(height: screenHeight * 0.01),
+//               CustomSubTitle(
+//                   color: Appcolor.black,
+//                   fontSize: 11,
+//                   subTitle: 'abouttext2'.tr,
+//                   maxLines: 2),
+//               SizedBox(height: screenHeight * 0.02),
+//               CustomTitle(
+//                   fontWeight: FontWeight.w500,
+//                   textHeading: 'abouttext3'.tr,
+//                   fontSize: 12,
+//                   color: Appcolor.buttonColor),
+//               SizedBox(height: screenHeight * 0.02),
+//               const VideoPlayer(),
+//               SizedBox(height: screenHeight * 0.02),
+//               CustomTitle(
+//                   fontWeight: FontWeight.w500,
+//                   textHeading: 'abouttext4'.tr,
+//                   fontSize: 14,
+//                   color: Appcolor.buttonColor),
+//               SizedBox(height: screenHeight * 0.01),
+//               CustomSubTitle(
+//                 color: Appcolor.black,
+//                 fontSize: 11,
+//                 subTitle: "abouttext5".tr,
+//                 maxLines: 14,
+//               ),
+//               SizedBox(height: screenHeight * 0.02),
+//               Aboutgridview(),
+//               SizedBox(height: screenHeight * 0.02),
+//               CustomTitle(
+//                   fontWeight: FontWeight.w500,
+//                   textHeading: 'abouttext10'.tr,
+//                   fontSize: 14,
+//                   color: Appcolor.buttonColor),
+//               SizedBox(height: screenHeight * 0.02),
+//               const Text(
+//                 'AL Ramool Oasis Building, Office #211, Floor 1, Umm Ramool, Dubai, UAE',
+//                 style: TextStyle(
+//                   color: Appcolor.black,
+//                   fontSize: 12,
+//                   fontWeight: FontWeight.w400,
+//                   decoration: TextDecoration.underline,
+//                 ),
+//               ),
+//               SizedBox(height: screenHeight * 0.01),
+//               const ContactInfo(text: '+971-50-6515493', text1: '– UAE'),
+//               const ContactInfo(text: '+971-50-6296037', text1: '– ABU DHABI'),
+//               const ContactInfo(text: '+971-50-6296247', text1: '– DUBAI'),
+//               const ContactInfo(text: '+971 4 331 9312', text1: '– Office'),
+//               SizedBox(height: screenHeight * 0.01),
+//               const EmailInfo(email: 'info@royalfujistar.com'),
+//               const EmailInfo(email: 'sales@royalfujistar.com'),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:royal_fuji_star/constants/size.dart';
@@ -14,83 +111,103 @@ class Aboutcontainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // height: screenHeight * 0.58,
-      height: ScreenSize.getHeight(context) * 0.58,
-      width: ScreenSize.getWidth(context),
-      decoration: const BoxDecoration(
-          color: Appcolor.white,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(45), topRight: Radius.circular(45))),
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: screenHeight * 0.02),
-              CustomTitle(
-                  fontWeight: FontWeight.w500,
-                  textHeading: 'abouttext1'.tr,
-                  fontSize: 16,
-                  color: Appcolor.buttonColor),
-              SizedBox(height: screenHeight * 0.01),
-              CustomSubTitle(
-                  color: Appcolor.black,
-                  fontSize: 11,
-                  subTitle: 'abouttext2'.tr,
-                  maxLines: 2),
-              SizedBox(height: screenHeight * 0.02),
-              CustomTitle(
-                  fontWeight: FontWeight.w500,
-                  textHeading: 'abouttext3'.tr,
-                  fontSize: 12,
-                  color: Appcolor.buttonColor),
-              SizedBox(height: screenHeight * 0.02),
-              const VideoPlayer(),
-              SizedBox(height: screenHeight * 0.02),
-              CustomTitle(
-                  fontWeight: FontWeight.w500,
-                  textHeading: 'abouttext4'.tr,
-                  fontSize: 14,
-                  color: Appcolor.buttonColor),
-              SizedBox(height: screenHeight * 0.01),
-              CustomSubTitle(
-                color: Appcolor.black,
-                fontSize: 11,
-                subTitle: "abouttext5".tr,
-                maxLines: 14,
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        if (orientation == Orientation.landscape) {
+          return const Scaffold(
+            body: VideoPlayer(),
+          );
+        } else {
+          return Container(
+            height: ScreenSize.getHeight(context) * 0.58,
+            width: ScreenSize.getWidth(context),
+            decoration: const BoxDecoration(
+              color: Appcolor.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(45),
+                topRight: Radius.circular(45),
               ),
-              SizedBox(height: screenHeight * 0.02),
-              Aboutgridview(),
-              SizedBox(height: screenHeight * 0.02),
-              CustomTitle(
-                  fontWeight: FontWeight.w500,
-                  textHeading: 'abouttext10'.tr,
-                  fontSize: 14,
-                  color: Appcolor.buttonColor),
-              SizedBox(height: screenHeight * 0.02),
-              const Text(
-                'AL Ramool Oasis Building, Office #211, Floor 1, Umm Ramool, Dubai, UAE',
-                style: TextStyle(
-                  color: Appcolor.black,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
-                  decoration: TextDecoration.underline,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.02),
+                    CustomTitle(
+                      fontWeight: FontWeight.w500,
+                      textHeading: 'abouttext1'.tr,
+                      fontSize: 16,
+                      color: Appcolor.buttonColor,
+                    ),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.01),
+                    CustomSubTitle(
+                      color: Appcolor.black,
+                      fontSize: 11,
+                      subTitle: 'abouttext2'.tr,
+                      maxLines: 2,
+                    ),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.02),
+                    CustomTitle(
+                      fontWeight: FontWeight.w500,
+                      textHeading: 'abouttext3'.tr,
+                      fontSize: 12,
+                      color: Appcolor.buttonColor,
+                    ),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.02),
+                    Container(
+                      height: ScreenSize.getHeight(context) * 0.25,
+                      child: const VideoPlayer(),
+                    ),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.02),
+                    CustomTitle(
+                      fontWeight: FontWeight.w500,
+                      textHeading: 'abouttext4'.tr,
+                      fontSize: 14,
+                      color: Appcolor.buttonColor,
+                    ),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.01),
+                    CustomSubTitle(
+                      color: Appcolor.black,
+                      fontSize: 11,
+                      subTitle: "abouttext5".tr,
+                      maxLines: 14,
+                    ),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.02),
+                    Aboutgridview(),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.02),
+                    CustomTitle(
+                      fontWeight: FontWeight.w500,
+                      textHeading: 'abouttext10'.tr,
+                      fontSize: 14,
+                      color: Appcolor.buttonColor,
+                    ),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.02),
+                    const Text(
+                      'AL Ramool Oasis Building, Office #211, Floor 1, Umm Ramool, Dubai, UAE',
+                      style: TextStyle(
+                        color: Appcolor.black,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.01),
+                    const ContactInfo(text: '+971-50-6515493', text1: '– UAE'),
+                    const ContactInfo(text: '+971-50-6296037', text1: '– ABU DHABI'),
+                    const ContactInfo(text: '+971-50-6296247', text1: '– DUBAI'),
+                    const ContactInfo(text: '+971 4 331 9312', text1: '– Office'),
+                    SizedBox(height: ScreenSize.getHeight(context) * 0.01),
+                    const EmailInfo(email: 'info@royalfujistar.com'),
+                    const EmailInfo(email: 'sales@royalfujistar.com'),
+                  ],
                 ),
               ),
-              SizedBox(height: screenHeight * 0.01),
-              const ContactInfo(text: '+971-50-6515493', text1: '– UAE'),
-              const ContactInfo(text: '+971-50-6296037', text1: '– ABU DHABI'),
-              const ContactInfo(text: '+971-50-6296247', text1: '– DUBAI'),
-              const ContactInfo(text: '+971 4 331 9312', text1: '– Office'),
-              SizedBox(height: screenHeight * 0.01),
-              const EmailInfo(email: 'info@royalfujistar.com'),
-              const EmailInfo(email: 'sales@royalfujistar.com'),
-            ],
-          ),
-        ),
-      ),
+            ),
+          );
+        }
+      },
     );
   }
 }
--- lib/screens/bottomnav/about/widgets/videoplayer.dart
+++ lib/screens/bottomnav/about/widgets/videoplayer.dart
@@ -1,3 +1,77 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+// class VideoPlayer extends StatefulWidget {
+//   const VideoPlayer({super.key});
+
+//   @override
+//   State<VideoPlayer> createState() => _VideoPlayerState();
+// }
+
+// class _VideoPlayerState extends State<VideoPlayer> {
+//   late YoutubePlayerController youtubePlayerController;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     youtubePlayerController = YoutubePlayerController(
+//       initialVideoId: 'dh7rRIZCni0',
+//       flags: const YoutubePlayerFlags(
+//         autoPlay: false,
+//         mute: true,
+//       ),
+//     );
+
+//     // Listen to orientation changes
+//     youtubePlayerController.addListener(() {
+//       if (youtubePlayerController.value.isFullScreen) {
+//         SystemChrome.setPreferredOrientations([
+//           DeviceOrientation.landscapeLeft,
+//           DeviceOrientation.landscapeRight,
+//         ]);
+//       } else {
+//         SystemChrome.setPreferredOrientations([
+//           DeviceOrientation.portraitUp,
+//         ]);
+//       }
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     // Reset preferred orientations on dispose
+//     SystemChrome.setPreferredOrientations([
+//       DeviceOrientation.portraitUp,
+//     ]);
+//     youtubePlayerController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // ignore: deprecated_member_use
+//     return WillPopScope(
+//       onWillPop: () async {
+//         // Reset preferred orientations when navigating back
+//         SystemChrome.setPreferredOrientations([
+//           DeviceOrientation.portraitUp,
+//         ]);
+//         return true;
+//       },
+//       child: Center(
+//         child: SizedBox(
+//           width: MediaQuery.of(context).size.width,
+//           child: YoutubePlayer(
+//             controller: youtubePlayerController,
+//             showVideoProgressIndicator: true,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
@@ -20,17 +94,20 @@ class _VideoPlayerState extends State<VideoPlayer> {
       flags: const YoutubePlayerFlags(
         autoPlay: false,
         mute: true,
+        hideThumbnail: true,
       ),
     );
 
     // Listen to orientation changes
     youtubePlayerController.addListener(() {
       if (youtubePlayerController.value.isFullScreen) {
+        SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
         SystemChrome.setPreferredOrientations([
           DeviceOrientation.landscapeLeft,
           DeviceOrientation.landscapeRight,
         ]);
       } else {
+        SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
         SystemChrome.setPreferredOrientations([
           DeviceOrientation.portraitUp,
         ]);
@@ -40,7 +117,8 @@ class _VideoPlayerState extends State<VideoPlayer> {
 
   @override
   void dispose() {
-    // Reset preferred orientations on dispose
+    // Reset preferred orientations and system UI on dispose
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
@@ -50,22 +128,44 @@ class _VideoPlayerState extends State<VideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    // ignore: deprecated_member_use
     return WillPopScope(
       onWillPop: () async {
-        // Reset preferred orientations when navigating back
+        // Reset preferred orientations and system UI when navigating back
+        SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
         SystemChrome.setPreferredOrientations([
           DeviceOrientation.portraitUp,
         ]);
         return true;
       },
-      child: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: YoutubePlayer(
-            controller: youtubePlayerController,
-            showVideoProgressIndicator: true,
-          ),
+      child: Scaffold(
+        body: OrientationBuilder(
+          builder: (context, orientation) {
+            return Stack(
+              children: [
+                // Black background
+                Container(
+                  color: orientation == Orientation.landscape
+                      ? Colors.black
+                      : Colors.white,
+                ),
+                // Center the video player
+                Center(
+                  child: SizedBox(
+                    width: orientation == Orientation.landscape
+                        ? MediaQuery.of(context).size.width
+                        : MediaQuery.of(context).size.width * 0.9,
+                    height: orientation == Orientation.landscape
+                        ? MediaQuery.of(context).size.height
+                        : MediaQuery.of(context).size.height * 0.5,
+                    child: YoutubePlayer(
+                      controller: youtubePlayerController,
+                      showVideoProgressIndicator: true,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
